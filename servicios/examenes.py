5ba2b4dad9288db6b4a1d98bde64eb1e657010bc
German <20245365@esen.edu.sv>
2026-07-03 22:00:23 -0600

    Clases de examenes.py

--- servicios/examenes.py
+++ servicios/examenes.py
@@ -1,15 +1,68 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from dominio.pacientes import Paciente
 from dominio.personal import Radiologo
-from dominio.clinica import Departamento, Municipio, Sucursal
-class Organo:
-    pass
+from dominio.clinica import  Sucursal
+from typing import Optional
+from datetime import date, time
 
-class OrganoExamen:
-    pass
-
+@dataclass
 class TipoExamen:
-    pass
+    nombre:str
+    descripcion:str
+    costo:float
+    preindicaciones:str
+    id_tipo_examen:Optional[int] = None
+    
 
+@dataclass
 class Examen:
-    pass
+    fecha:date
+    hora:time
+    estado_examen:str
+    tipo_examen:TipoExamen
+    paciente:Paciente
+    radiologo:Radiologo
+    sucursal:Sucursal
+    doctor_referencia:str = ""
+    diagnostico:str = ""
+    sugerencia:str = ""
+    observaciones:str = ""
+    organos:list[OrganoExamen] = field(default_factory=list)
+    imagenes:list[ImagenExamen] = field(default_factory=list)
+    id_examen:Optional[int] = None
+    
+    def post__init__(self):
+        estados = ["Pendiente", "Hecho", "Cancelado", "Reprogramado", "Seguimiento"]
+        if self.estado_examen.capitalize() not in estados:
+            raise ValueError(f"Estado de examen inválido. \nEstados de examen válidos:{estados}")
+        if len(self.doctor_referencia) > 50:
+            raise ValueError("El nombre del doctor de referencia no puede superar los 50 caracteres.")
+
+    def __eq__(self, other):
+        if not isinstance(other, Examen):
+            return False
+        return self.id_examen == other.id_examen
+
+@dataclass
+class OrganoExamen:
+    nombre_organo:str
+    longitud_organo:float
+    ancho_organo:float
+    volumen_organo:float
+    id_organo_examen:Optional[int] = None
+    
+    def post__init__(self):
+        organos = ["Riñon izquierdo", "Riñon derecho", "Útero", "Hígado", "Próstata", "Vejiga", "Páncreas"]
+        if self.nombre_organo.capitalize() not in organos:
+            raise ValueError(f"Órgano inválido. \nÓrganos válidos:{organos}")
+        
+
+    def __eq__(self, other):
+        if not isinstance(other, OrganoExamen):
+            return False
+        return self.id_organo_examen == other.id_organo_examen
+    
+@dataclass(frozen=True)
+class ImagenExamen:
+    url:str
+    descripcion:str
